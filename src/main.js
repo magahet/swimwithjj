16af00f3ed03d55e1564ad9a0ee413e1e3d8e7e9
Gar Mendiola <magahet@gmail.com>
2018-04-21 15:23:16 -0700

    switched to full webpack template

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
-import BootstrapVue from "bootstrap-vue"
-import App from './App.vue'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap-vue/dist/bootstrap-vue.css"
+import App from './App'
+import router from './router'
 
-Vue.use(BootstrapVue)
+Vue.config.productionTip = false
 
+/* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
